20b2c1727bc90db318bc8f760bd35f670ab7fefd
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2019-04-12 12:42:40 -0500

    Basic template server

---
+++ server/main.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import path from 'path'
+
+const app = new express()
+
+app.set('views', path.join(__dirname, '../client/views'))
+app.set('view engine', 'pug')
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request received for: ${req.originalUrl}`)
+    next()
+})
+
+// Render and serve pug views
+app.get('/', (req, res) => {
+    res.render('index', { title: 'index page' })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About page'})
+})
+app.get('/test', (req, res) => {
+    res.render('test', { title: 'View test', message: 'Views appear to be working' })
+})
+
+// Serve public static files
+app.use(express.static(path.join(__dirname, '../client/static')))
+
+app.listen(80, () => {
+    console.log('Server is now listening on port 80.')
+})
